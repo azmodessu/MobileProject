ce01b531fecf78c2f3f917cc23308be9db4ac8c3
Azmodessu <golovrkd@yandex.ru>
2024-02-28 20:45:52 +0300

    first practice done

--- control_lesson1/src/main/java/ru/mirea/golovds/control_lesson1/MainActivity.java
+++ control_lesson1/src/main/java/ru/mirea/golovds/control_lesson1/MainActivity.java
@@ -3,12 +3,22 @@ package ru.mirea.golovds.control_lesson1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.textView);
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        myTextView.setText("New text in MIREA");
+        Button button = findViewById(R.id.button);
+        button.setText("MireaButton");
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setChecked(true);
     }
 }
